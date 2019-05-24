8e26453d2198db3884d7d5bba7ae7c33540b57e6
jinjin1234 <18792140045@163.com>
2019-05-24 10:10:49 +0800

    springcloud练习

---
+++ service-ribbon/src/test/java/com/ribon/RibonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ribon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RibonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
