689db0992ff969e01707682a7c80701eeff041cb
zhangyazhou <zyzme124@163.com>
2019-05-24 15:03:46 +0800

    sss

--- service-ribbon/src/main/java/com/ribon/service/HelloService.java
+++ service-ribbon/src/main/java/com/ribon/service/HelloService.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class HelloService {
     @Autowired
-    RestTemplate restTemplate;
+    RestTemplate restTemplate;//
 
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
